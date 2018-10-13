adfed51ab339d2308773e99548d4f1dc75ff5a44
Rasmus Kröger <rasmuskroger@gmail.com>
2018-10-13 20:08:51 +0300

    Add dockerfile, socket io

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SocketProvider } from 'socket.io-react';
+import io from 'socket.io-client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const socket = io.connect('http://localhost:3000');
+socket.on('kuhler', (msg) => console.log(msg));
+
+
+ReactDOM.render(
+	<SocketProvider socket={socket}>
+		<App />
+	</SocketProvider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
